a5263a9a11595ab32d9c6e00cd11f6c610ac0d8a
David Delgado <david.delgadop88@gmail.com>
2024-02-17 11:04:40 -0400

    mvpv1.1, refactored JS, added random color mode, clear button, more styling

--- README.md
+++ README.md
@@ -1,20 +1,27 @@
 # Etch-a-Sketch
-### MVP v1
+### MVP v1.1
 
-![mvp1-preview](./mvp-v1.png)
+![mvp2-preview](./mvp-v1.1.png)
 
-This webpage is a digital version of the classic Etch-a-Sketch toy. It allows users to draw on a grid of square divs by hovering the mouse over them. The grid size can be changed dynamically based on user input.
+This webpage is a digital version of the classic Etch-a-Sketch toy. It allows users to draw on a grid of square divs by hovering the mouse over them. The grid size can be changed dynamically based on user input. Part of the challenge was to render the grid using flexbox instead of css grid.
 
 ## Technologies Used
 - HTML
 - CSS (Flexbox)
 - JavaScript
 
+## Features
+- Paint random colors with the "Random" button.
+- Clean your drawing.
+- Resize the grid.
+
 ## How to Use
-- The initial grid size is set to 9x9.
+- The initial grid size is set to 16x16.
 - Hover over the grid squares to draw on them.
 - Use the input field and the "Resize grid!" button to change the - size of the grid.
 - Click the "Resize grid!" button to generate a new grid with the specified size.
+- Click the "Clear" button to clean up your drawing.
+- Click the "Random" button to enable random color painting, you can also go back to the standard color mode.
 
 ## What I learnt
 - Creating HTML elements with JS.
@@ -26,4 +33,8 @@ This webpage is a digital version of the classic Etch-a-Sketch toy. It allows us
 ## Challenges and Future Improvements
 - The current implementation does not include advanced styling and is focused on functionality.
 - The JavaScript code is to be refactored to improve readability and maintainability.
-- Additional features are to be added, such as different pen colors, eraser functionality, random color mode.
+- Additional features are to be added.
+
+## Previous Versions Screenshots
+### v1.0
+![mvp1-preview](./mvp-v1.png)
--- index.js
+++ index.js
@@ -1,51 +1,91 @@
-const INITIAL_SIZE = 9;
+const INITIAL_NxN_SIZE = 16;
+const INITIAL_COLOR = "#663399";
+const INITIAL_MODE = "color";
 
+// Elements
 const sketchpad = document.getElementById('sketchpad');
-console.log(sketchpad);
+const clearBtn = document.getElementById('clear-btn');
+const resizeBtn = document.getElementById('resize-btn');
+const sizeInput = document.querySelector('input');
+const colorModeBtn = document.getElementById('color-mode-btn');
+const randomModeBtn = document.getElementById('random-mode-btn');
+
+// Events
+clearBtn.addEventListener('click', clearGrid);
+resizeBtn.addEventListener('click', changeSize);
+sketchpad.addEventListener('mouseenter', paint);
+colorModeBtn.addEventListener('click', changeToColorMode);
+randomModeBtn.addEventListener('click', changeToRandomMode);
+
+let currentMode = INITIAL_MODE;
+
+function changeToColorMode() {
+    if (currentMode !== "color") {
+        currentMode = "color";
+        randomModeBtn.classList.remove('active');
+        colorModeBtn.classList.add('active');
+    }   
+    return;
+}
+
+function changeToRandomMode() {
+    if (currentMode !== "random") {
+        currentMode = "random";
+        randomModeBtn.classList.add('active');
+        colorModeBtn.classList.remove('active');
+    }
+    return;
+}
 
 function generateGrid(size) {
+    const squareHeightPixels = `${600/size}px`;
+    
     for (let i = 0; i < size; i++) {
         const row = document.createElement("div");
         row.classList.add('row');
-        row.style.height = `${600/size}px`;
-        sketchpad.appendChild(row);
-    }
-    for (let i = 0; i < size; i++) {
-        const rows = document.querySelectorAll('.row');
-        console.log(rows);
-        rows.forEach(row => {
+        row.style.height = squareHeightPixels;
+        for (let j = 0; j < size; j++) {
             const col = document.createElement("div");
             col.classList.add('col');
-            col.style.width = `${600/size}px`;
             row.appendChild(col);
-        })
+        }
+        sketchpad.appendChild(row);
     }
 }
 
-generateGrid(INITIAL_SIZE);
-
-function cleanGrid() {
-    sketchpad.replaceChildren();
-}
-
-function paint() {
-    const items = document.querySelectorAll('.col');
-    items.forEach(item => {
-        item.addEventListener('mouseenter', (e) => {
-            e.target.style.backgroundColor = '#663399';
-        })
-    })
+function clearGrid() {
+    const cols = document.querySelectorAll('.col');
+    cols.forEach(col => col.style.backgroundColor = "white");
 }
 
 function changeSize() {
     const size = parseInt(sizeInput.value);
-    cleanGrid();
+    if (size > 64 || size < 1 || !Number.isInteger(size)) {
+        return;
+    }
+    sketchpad.replaceChildren();
     generateGrid(size);
-    console.log(size)
 }
 
-const sizeInput = document.querySelector('input');
-const changeBtn = document.getElementById('changebtn');
+function generateRandomColor() {
+    const R = Math.floor(Math.random() * 256);
+    const G = Math.floor(Math.random() * 256);
+    const B = Math.floor(Math.random() * 256);
+    return `rgb(${R}, ${G}, ${B})`;
+}
 
-changeBtn.addEventListener('click', changeSize);
-sketchpad.addEventListener('mouseenter', paint);
+function paint() {
+    const gridSquares = document.querySelectorAll('.col');
+    gridSquares.forEach((gridSquare) => {
+        gridSquare.addEventListener('mouseenter', (e) => {
+            if (currentMode === "color") {
+                e.target.style.backgroundColor = "#663399";
+            } else {
+                e.target.style.backgroundColor = generateRandomColor();
+            } 
+        })
+    })
+}
+
+/// Run app
+generateGrid(INITIAL_NxN_SIZE);
